4ccb33b9f6fde20b712f5923f0d86dccc7fc932a
Space Analyzer Developer <developer@spaceanalyzer.local>
2026-04-27 23:56:10 -0700

    feat: Add animations and polish to Vue 3 redesign
    - Fixed TypeScript errors in FileBrowserView (added FileItem interface)
    - Created Loading component with spinner animation
    - Added page transition animations to AppShell (fade + slide)
    - Updated design system exports
    
    Build: Successful (55 modules, 2.74s)
    Bundle: 147KB CSS, 37KB main JS, 271KB vendor JS

--- src/design-system/components/index.ts
+++ src/design-system/components/index.ts
@@ -1,3 +1,4 @@
 // Design System Components - Barrel Export
-export { default as Button } from './Button.vue'
-export { default as Card } from './Card.vue'
+export { default as Button } from "./Button.vue";
+export { default as Card } from "./Card.vue";
+export { default as Loading } from "./Loading.vue";
